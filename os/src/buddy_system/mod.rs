dca1e582ec56e79b1a255ea86c25545d20983aa8
Jiang Sheng <jiangshengdev@outlook.com>
2025-05-17 07:08:24 +0800

    annotation

--- os/src/buddy_system/mod.rs
+++ os/src/buddy_system/mod.rs
@@ -7,18 +7,19 @@ use core::mem::size_of;
 use core::ptr::NonNull;
 pub use test::test_all;
 
+/// 伙伴系统堆，最大阶数为 `ORDER - 1`
 pub struct Heap<const ORDER: usize> {
-    // buddy system with max order of `ORDER - 1`
+    // 伙伴系统的空闲链表，每个阶对应一个链表
     free_list: [linked_list::LinkedList; ORDER],
 
-    // statistics
-    user: usize,
-    allocated: usize,
-    total: usize,
+    // 统计信息
+    user: usize,      // 用户实际请求的字节数
+    allocated: usize, // 实际分配的字节数
+    total: usize,     // 堆总字节数
 }
 
 impl<const ORDER: usize> Heap<ORDER> {
-    /// Create an empty heap
+    /// 创建一个空堆
     pub const fn new() -> Self {
         Heap {
             free_list: [linked_list::LinkedList::new(); ORDER],
@@ -28,15 +29,15 @@ impl<const ORDER: usize> Heap<ORDER> {
         }
     }
 
-    /// Create an empty heap
+    /// 创建一个空堆（别名）
     #[allow(unused)]
     pub const fn empty() -> Self {
         Self::new()
     }
 
-    /// Add a range of memory [start, end) to the heap
+    /// 向堆中添加一段内存区间 [start, end)
     pub unsafe fn add_to_heap(&mut self, mut start: usize, mut end: usize) {
-        // avoid unaligned access on some platforms
+        // 避免某些平台上的未对齐访问
         start = (start + size_of::<usize>() - 1) & (!size_of::<usize>() + 1);
         end &= !size_of::<usize>() + 1;
         assert!(start <= end);
@@ -48,8 +49,7 @@ impl<const ORDER: usize> Heap<ORDER> {
             let lowbit = current_start & (!current_start + 1);
             let mut size = min(lowbit, prev_power_of_two(end - current_start));
 
-            // If the order of size is larger than the max order,
-            // split it into smaller blocks.
+            // 如果 size 的阶数大于最大阶数，则拆分为更小的块
             let mut order = size.trailing_zeros() as usize;
             if order > ORDER - 1 {
                 order = ORDER - 1;
@@ -64,13 +64,13 @@ impl<const ORDER: usize> Heap<ORDER> {
         self.total += total;
     }
 
-    /// Add a range of memory [start, start+size) to the heap
+    /// 向堆中添加一段内存区间 [start, start+size)
     #[allow(unused)]
     pub unsafe fn init(&mut self, start: usize, size: usize) {
         self.add_to_heap(start, start + size);
     }
 
-    /// Alloc a range of memory from the heap satifying `layout` requirements
+    /// 从堆中分配一段满足 `layout` 要求的内存
     pub fn alloc(&mut self, layout: Layout) -> Result<NonNull<u8>, ()> {
         let size = max(
             layout.size().next_power_of_two(),
@@ -78,9 +78,9 @@ impl<const ORDER: usize> Heap<ORDER> {
         );
         let class = size.trailing_zeros() as usize;
         for i in class..self.free_list.len() {
-            // Find the first non-empty size class
+            // 找到第一个非空的 size class
             if !self.free_list[i].is_empty() {
-                // Split buffers
+                // 拆分更大的块
                 for j in (class + 1..i + 1).rev() {
                     if let Some(block) = self.free_list[j].pop() {
                         unsafe {
@@ -111,7 +111,7 @@ impl<const ORDER: usize> Heap<ORDER> {
         Err(())
     }
 
-    /// Dealloc a range of memory from the heap
+    /// 释放一段内存回堆
     pub fn dealloc(&mut self, ptr: NonNull<u8>, layout: Layout) {
         let size = max(
             layout.size().next_power_of_two(),
@@ -120,10 +120,10 @@ impl<const ORDER: usize> Heap<ORDER> {
         let class = size.trailing_zeros() as usize;
 
         unsafe {
-            // Put back into free list
+            // 放回空闲链表
             self.free_list[class].push(ptr.as_ptr() as *mut usize);
 
-            // Merge free buddy lists
+            // 合并伙伴块
             let mut current_ptr = ptr.as_ptr() as usize;
             let mut current_class = class;
 
@@ -138,7 +138,7 @@ impl<const ORDER: usize> Heap<ORDER> {
                     }
                 }
 
-                // Free buddy found
+                // 找到空闲伙伴块
                 if flag {
                     self.free_list[current_class].pop();
                     current_ptr = min(current_ptr, buddy);
@@ -154,25 +154,26 @@ impl<const ORDER: usize> Heap<ORDER> {
         self.allocated -= size;
     }
 
-    /// Return the number of bytes that user requests
+    /// 返回用户实际请求的字节数
     #[allow(unused)]
     pub fn stats_alloc_user(&self) -> usize {
         self.user
     }
 
-    /// Return the number of bytes that are actually allocated
+    /// 返回实际分配的字节数
     #[allow(unused)]
     pub fn stats_alloc_actual(&self) -> usize {
         self.allocated
     }
 
-    /// Return the total number of bytes in the heap
+    /// 返回堆的总字节数
     #[allow(unused)]
     pub fn stats_total_bytes(&self) -> usize {
         self.total
     }
 }
 
+/// 计算小于等于 num 的最大 2 的幂
 pub(crate) fn prev_power_of_two(num: usize) -> usize {
     1 << (usize::BITS as usize - num.leading_zeros() as usize - 1)
 }
